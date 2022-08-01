a41a242f01c0eb32ddd5664cc2471bdbaf0d7187
Nicolas Marra <68780542+nicolasmarra@users.noreply.github.com>
2022-08-01 15:35:31 +0200

    update menu move

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function scroller() {
-    if (scrollY != 0) naviguer.classList.add('scroll')
-    else naviguer.classList.remove('scroll')
-  }
-  
-  function ouvrirMenu() {
-    document.body.classList.add('menu-etendu')
-  }
-  
-  function fermerMenu() {
+  if (scrollY > 0) naviguer.classList.add('scroll')
+  else naviguer.classList.remove('scroll')
+}
+
+function onMenu() {
+  if (document.body.classList.contains('menu-etendu'))
     document.body.classList.remove('menu-etendu')
-  }
-  
+  else document.body.classList.add('menu-etendu')
+}
